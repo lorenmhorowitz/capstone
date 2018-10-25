6aebf872e866f5a49782206b77e2970a977f8155
Max Ellis <max.ellis@wsu.edu>
2018-10-24 18:46:36 -0700

    Fixed positioning of AppBar and tested

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -5,7 +5,21 @@ import Toolbar from "@material-ui/core/Toolbar";
 
 import { withStyles } from "@material-ui/core/styles";
 
-const styles = {};
+const styles = {
+  root: {
+    position: "absolute",
+    left: 0,
+    right: 0,
+    top: 0
+  },
+  grow: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  }
+};
 
 class HeaderBar extends Component {
   render() {
