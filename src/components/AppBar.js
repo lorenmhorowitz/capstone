a1e2228d06186aac6af9a8db1da2c2856f003045
Max Ellis <max.ellis@wsu.edu>
2018-10-24 18:36:15 -0700

    Added basic structure to the AppBar

--- src/components/AppBar.js
+++ src/components/AppBar.js
@@ -1,1 +1,25 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = {};
+
+class HeaderBar extends Component {
+  render() {
+    return (
+      <div>
+        <AppBar>
+          <Toolbar />
+        </AppBar>
+      </div>
+    );
+  }
+}
+HeaderBar.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(HeaderBar);
