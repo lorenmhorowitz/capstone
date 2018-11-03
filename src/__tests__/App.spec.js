06571932e63188c35a3b20c286b7eeafe42047a2
Chayce Heiberg <theheibergproject@gmail.com>
2018-11-03 14:13:27 -0700

    Add app test file

--- src/__tests__/App.spec.js
+++ src/__tests__/App.spec.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "../client/App";
+import App from "../App";
 
 describe("App ", () => {
   it("renders without crashing", () => {
