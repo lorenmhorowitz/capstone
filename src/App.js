5ddfd58f6b9c6d9f9c72f6f15c99dd688f887e63
Arnold Castro <arnold.castro@wsu.edu>
2018-11-03 13:27:52 -0700

    Added bordered box.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import "./css/App.css";
+import Login from "./login/Login";
 
 class App extends Component {
   render() {
-    return <div className="App" />;
+    return (
+      <div>
+        <Login />
+      </div>
+    );
   }
 }
 
---
+++ src/login/Login.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "../css/Login.css";
+
+class Login extends Component {
+  render() {
+    return <div id="loginSquare" />;
+  }
+}
+
+export default Login;
