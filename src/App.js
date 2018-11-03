ff24734442a19a41969a1f27bff6bb42ef8b6e62
Max Ellis <max.ellis@wsu.edu>
2018-11-03 12:27:44 -0700

    Removed test code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React, { Component } from "react";
 import "./css/App.css";
-import AppBar from "./components/AppBar";
+
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        ;<AppBar />
-      </div>
-    );
+    return <div className="App" />;
   }
 }
 
