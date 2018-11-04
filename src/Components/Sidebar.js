f472545513db1327c41378e4c43dc5c17c781928
Josh Feener <maranathaman7@gmail.com>
2018-11-03 18:33:02 -0700

    Intermediate changes of creating SideBar.css

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -9,26 +9,18 @@ import InboxIcon from "@material-ui/icons/MoveToInbox";
 import MailIcon from "@material-ui/icons/Mail";
 import ChevronRightIcon from "@material-ui/icons/ChevronRight";
 import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
-
-const drawerWidth = 240;
+import "../css/SideBar.css";
 
 const styles = theme => ({
-  root: {
-    display: "flex",
-  },
-  drawerPaper: {
-    top: "70px", //moves Sidebar below AppBar
-    bottom: "70px",
-    position: "fixed",
+  drawerPaperTransitions: {
     whiteSpace: "nowrap", //text doesn't shrink into side
-    width: drawerWidth,
     transition: theme.transitions.create("width", {
       //makes transitions smooth
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen
     })
   },
-  drawerPaperClose: {
+  drawerPaperCloseTransitions: {
     overflowX: "hidden", //display mini sidebar
     width: theme.spacing.unit * 7,
     [theme.breakpoints.up("sm")]: {
@@ -38,13 +30,6 @@ const styles = theme => ({
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen
     })
-  },
-  iconButton: { //fixes spacing 
-    marginTop: "15px",
-    marginBottom: "7px"
-  },
-  listItem: { 
-    height: "75px"
   }
 });
 
@@ -62,23 +47,22 @@ class Sidebar extends Component {
     const { open } = this.state;
 
     return (
-      <div className={classes.root}>
+      <div className="root">
         <Drawer
           variant="permanent"
           anchor="left"
           open={open}
           classes={{
-            paper: classNames(
-              classes.drawerPaper,
-              !open && classes.drawerPaperClose
-            )
+            paper: classNames( 
+              classes.drawerPaperTransitions,
+              !open && classes.drawerPaperCloseTransitions
+            ) 
           }}
-          className="drawer"
         >
           <div>
             <Divider />
             <IconButton
-              className={classes.iconButton}
+              className="iconButton"
               onClick={this.handleSidebarToggle}
             >
               {open === false ? <ChevronRightIcon /> : <ChevronLeftIcon />}
@@ -86,35 +70,35 @@ class Sidebar extends Component {
           </div>
           <List>
             <Divider />
-            <ListItem className={classes.listItem} button>
+            <ListItem className="listItem" button>
               <ListItemIcon>
                 <InboxIcon />
               </ListItemIcon>
               <ListItemText primary="Info" />
             </ListItem>
             <Divider />
-            <ListItem className={classes.listItem} button>
+            <ListItem className="listItem" button>
               <ListItemIcon>
                 <MailIcon />
               </ListItemIcon>
               <ListItemText primary="Roofing" />
             </ListItem>
             <Divider />
-            <ListItem className={classes.listItem} button>
+            <ListItem className="listItem" button>
               <ListItemIcon>
                 <InboxIcon />
               </ListItemIcon>
               <ListItemText primary="Siding" />
             </ListItem>
             <Divider />
-            <ListItem className={classes.listItem} button>
+            <ListItem className="listItem" button>
               <ListItemIcon>
                 <MailIcon />
               </ListItemIcon>
               <ListItemText primary="Windows" />
             </ListItem>
             <Divider />
-            <ListItem className={classes.listItem} button>
+            <ListItem className="listItem" button>
               <ListItemIcon>
                 <MailIcon />
               </ListItemIcon>
