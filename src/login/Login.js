bad2f728e7497fe545e9b4c3529a77c156b8e4b4
Arnold Castro <arnold.castro@wsu.edu>
2018-11-06 12:30:14 -0800

    Finallized login page look.

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "../css/Login.css";
 import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
+import TextField from "../components/TextField";
 
 class Login extends Component {
   render() {
@@ -12,19 +12,11 @@ class Login extends Component {
             <span id="Corner">Corner</span>
             <span id="Stone">Stone</span>
           </div>
-          <div id="Info">
-            Please log in with your Hover credentials using the buttom below.
-          </div>
-          <form>
-            <TextField id="field" label="email" />
-          </form>
-          <TextField
-            id="field"
-            margin="normal"
-            variant="outlined"
-            name="email"
-            label="email"
-          />
+
+          <TextField label="email" />
+
+          <TextField label="password" />
+
           <Button id="Login">Log In</Button>
           <Button id="HoverLogin">Login With Hover</Button>
         </div>
