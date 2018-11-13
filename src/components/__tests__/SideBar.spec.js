bdc1be62d146e0ea1ed584d10aa3b118eb5d029d
Loren Horowitz <lorenmhorowitz@gmail.com>
2018-11-13 14:09:35 -0800

    Updated SideBar test.

--- src/components/__tests__/SideBar.spec.js
+++ src/components/__tests__/SideBar.spec.js
@@ -1,12 +1,10 @@
 import React from "react";
 import SideBar from "../SideBar";
 import { shallow } from "enzyme";
-import { styles } from "../../css/SideBarcss";
 
 describe("SideBar ", () => {
   it("Sidebar snapshot test successful", () => {
-    const classes = { styles };
-    const sidebar = shallow(<SideBar classes={classes} />);
+    const sidebar = shallow(<SideBar />);
     expect(sidebar).toMatchSnapshot();
   });
 });
