208df5b12337ff6eb02b5ae9510a83ebb373de50
Josh Feener <maranathaman7@gmail.com>
2018-11-15 16:01:19 -0800

    Changed margins

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -29,7 +29,7 @@ class JobCard extends Component {
             image={this.props.image}
             title="Nice house"
           />
-          <CardContent className="cardContent">
+          <CardContent>
             <Typography variant="subtitle2" align="left">
               {this.props.name}
             </Typography>
