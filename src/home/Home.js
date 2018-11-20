8cad1dec1ac3c99adad7051c281e0dbf603911e6
Loren Horowitz <lorenmhorowitz@gmail.com>
2018-11-20 11:33:33 -0800

    Testing failing on build steps.

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import AppBar from "../components/AppBar";
 import Loading from "../components/Loading";
-import SideBar from "../components/SideBar";
 
 class Home extends Component {
   state = {
@@ -12,7 +11,6 @@ class Home extends Component {
     return (
       <div>
         <AppBar />
-        <SideBar />
         {this.state.loading ? <Loading /> : null}
       </div>
     );
