80f136deb2cf4275e8d193ef4fbaed0a7455558b
Josh Feener <maranathaman7@gmail.com>
2018-12-02 13:21:36 -0800

    Job Cards are rendered in Grid

--- src/home/Home.js
+++ src/home/Home.js
@@ -8,7 +8,9 @@ import { mockJobsList } from "../../mocks/mockJobsList";
 
 class Home extends Component {
   state = {
-    loading: true
+    loading: true,
+    dataArr: mockJobsList,
+    jobCardArray: []
   };
 
   render() {
@@ -17,13 +19,16 @@ class Home extends Component {
         <AppBar />
         {this.state.loading ? <Loading /> : null}
         <GridList className={this.props.GridList}>
-          <JobCard
-            name={mockJobsList.results[0].name}
-            location_line_1={mockJobsList.results[0].location_line_1}
-            location_city={mockJobsList.results[0].location_city}
-            location_region={mockJobsList.results[0].location_region}
-            image="https://upload.wikimedia.org/wikipedia/commons/2/25/Houses_on_Garfield_Place,_Poughkeepsie,_NY.jpg"
-          />
+          {this.state.dataArr.results.map(data => (
+            <JobCard
+              key={data.id}
+              name={data.name}
+              location_line_1={data.location_line_1}
+              location_city={data.location_city}
+              location_region={data.location_region}
+              image="https://upload.wikimedia.org/wikipedia/commons/2/25/Houses_on_Garfield_Place,_Poughkeepsie,_NY.jpg"
+            />
+          ))}
         </GridList>
       </div>
     );
