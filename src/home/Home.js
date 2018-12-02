74af81cc2876de0bb8637389a36b5d6bf62cdf8d
Arnold Castro <arnold.castro@wsu.edu>
2018-12-02 12:53:06 -0800

    Started grid in home page.

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 import AppBar from "../components/AppBar";
 import Loading from "../components/Loading";
+import "../css/home.css";
+import { GridList } from "@material-ui/core";
+import JobCard from "../components/JobCard";
+import { mockJobsList } from "../../mocks/mockJobsList";
 
 class Home extends Component {
   state = {
@@ -12,6 +16,15 @@ class Home extends Component {
       <div>
         <AppBar />
         {this.state.loading ? <Loading /> : null}
+        <GridList className={this.props.GridList}>
+          <JobCard
+            name={mockJobsList.results[0].name}
+            location_line_1={mockJobsList.results[0].location_line_1}
+            location_city={mockJobsList.results[0].location_city}
+            location_region={mockJobsList.results[0].location_region}
+            image="https://upload.wikimedia.org/wikipedia/commons/2/25/Houses_on_Garfield_Place,_Poughkeepsie,_NY.jpg"
+          />
+        </GridList>
       </div>
     );
   }
