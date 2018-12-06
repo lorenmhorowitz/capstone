eba5d6bb62ffd19f5b8d260a0a49d72238a6f3c3
Josh Feener <maranathaman7@gmail.com>
2018-12-06 15:53:51 -0800

    Adding mock text to jobs page

--- src/Job/Job.js
+++ src/Job/Job.js
@@ -49,7 +49,31 @@ class Job extends Component {
           </div>
           <Divider id="bar1" />
           <div>
-            <p />
+            <p id="para">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              mattis tellus quis turpis gravida sollicitudin. Quisque vehicula
+              lorem consectetur vehicula imperdiet. Nullam non suscipit ex, et
+              dictum tortor. Vivamus eget volutpat elit. Fusce posuere finibus
+              laoreet. Donec tempus orci nec elit condimentum rhoncus. Etiam
+              diam nunc, rutrum sed condimentum nec, gravida ut metus. Nullam
+              tempus aliquet ante sit amet facilisis. Ut et finibus massa.
+              Pellentesque eget elit a neque venenatis scelerisque. Maecenas nec
+              nunc massa. Phasellus eget velit vitae orci gravida venenatis in
+              quis nisl. Phasellus non lectus eu velit tristique suscipit quis
+              nec enim. Quisque venenatis ultricies tortor maximus maximus.
+              Aenean ut pharetra nisl. Quisque porttitor, lorem et euismod
+              congue, felis turpis hendrerit sem, quis tempor purus nisl
+              accumsan dolor. Suspendisse fringilla ex vitae nisl convallis
+              ornare. Quisque porta tempus sollicitudin. Quisque suscipit velit
+              ut elit hendrerit, in varius mauris malesuada. Aenean vitae
+              elementum libero. Sed dignissim porttitor nisl, viverra ornare
+              erat porttitor sit amet. In tincidunt orci nibh, ut fermentum urna
+              volutpat non. Ut condimentum accumsan neque ut facilisis. Nunc
+              congue luctus est a lacinia. Cras a ultricies erat, et tincidunt
+              elit. Suspendisse eget mi vel leo luctus dapibus. Praesent
+              convallis arcu vitae dolor sagittis, porta euismod quam aliquam.
+              Sed vitae sollicitudin eros.
+            </p>
           </div>
 
           <div ref={this.sidingRef}>
@@ -57,14 +81,37 @@ class Job extends Component {
           </div>
           <Divider id="bar1" />
           <div>
-            <p />
+            <p id="para">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              mattis tellus quis turpis gravida sollicitudin. Quisque vehicula
+              lorem consectetur vehicula imperdiet. Nullam non suscipit ex, et
+              dictum tortor. Vivamus eget volutpat elit. Fusce posuere finibus
+              laoreet. Donec tempus orci nec elit condimentum rhoncus. Etiam
+              diam nunc, rutrum sed condimentum nec, gravida ut metus. Nullam
+              tempus aliquet ante sit amet facilisis. Ut et finibus massa.
+              Pellentesque eget elit a neque venenatis scelerisque. Maecenas nec
+              nunc massa. Phasellus eget velit vitae orci gravida venenatis in
+              quis nisl. Phasellus non lectus eu velit tristique suscipit quis
+              nec enim. Quisque venenatis ultricies tortor maximus maximus.
+              Aenean ut pharetra nisl. Quisque porttitor, lorem et euismod
+              congue, felis turpis hendrerit sem, quis tempor purus nisl
+              accumsan dolor. Suspendisse fringilla ex vitae nisl convallis
+              ornare. Quisque porta tempus sollicitudin. Quisque suscipit velit
+              ut elit hendrerit, in varius mauris malesuada. Aenean vitae
+              elementum libero. Sed dignissim porttitor nisl, viverra ornare
+              erat porttitor sit amet. In tincidunt orci nibh, ut fermentum urna
+              volutpat non. Ut condimentum accumsan neque ut facilisis. Nunc
+              congue luctus est a lacinia. Cras a ultricies erat, et tincidunt
+              elit. Suspendisse eget mi vel leo luctus dapibus. Praesent
+              convallis arcu vitae dolor sagittis, porta euismod quam aliquam.
+              Sed vitae sollicitudin eros.
+            </p>
           </div>
           <div ref={this.windowsRef}>
-            <Typography id="windows" variant="h4">
-              Windows Information
-            </Typography>
+            <Typography id="header">Windows Information</Typography>
           </div>
-          <p>
+          <Divider id="bar1" />
+          <p id="para">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
             mattis tellus quis turpis gravida sollicitudin. Quisque vehicula
             lorem consectetur vehicula imperdiet. Nullam non suscipit ex, et
@@ -89,7 +136,6 @@ class Job extends Component {
             euismod quam aliquam. Sed vitae sollicitudin eros.
           </p>
 
-          <Typography id="header">Windows Information</Typography>
           <Divider id="bar1" />
           <div />
         </div>
