5614397d24c53db344f5d57b6c350f0aaf4a4a48
Loren Horowitz <lorenmhorowitz@gmail.com>
2018-12-06 17:01:05 -0800

    Remove global window.

--- src/Job/__tests__/Job.spec.js
+++ src/Job/__tests__/Job.spec.js
@@ -2,8 +2,6 @@ import React from "react";
 import Job from "../Job";
 import { shallow } from "enzyme";
 
-global.window = { location: { pathname: "/job/2" } };
-
 describe.skip("Job Component", () => {
   it("renders without crashing", () => {
     let wrapper = shallow(<Job />);
