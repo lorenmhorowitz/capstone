692e16ca7e8ff5f6045fb8b4c0803609b6b4c2cd
Loren Horowitz <lorenmhorowitz@gmail.com>
2019-03-25 17:39:34 -0700

    Include header pricing subtotals.

--- src/utils/__tests__/formatter.spec.js
+++ src/utils/__tests__/formatter.spec.js
@@ -3,6 +3,6 @@ import formatter from "../formatter";
 const mockNum = 30205.1;
 
 it("will format the number correctly", () => {
-  let mockFormatted = formatter.format(mockNum);
+  let mockFormatted = formatter.money(mockNum);
   expect(mockFormatted).toEqual("30,205.10");
 });
--- src/utils/formatter.js
+++ src/utils/formatter.js
@@ -1,7 +1,24 @@
-const format = num => {
-  return num.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+const address = (location_city, location_region, location_postal_code) => {
+  let res = "";
+  if (location_city !== null && location_city !== "") {
+    res += location_city;
+  }
+  if (location_region !== null && location_region !== "") {
+    if (res !== "") res += `, `;
+    res += location_region;
+  }
+  if (location_postal_code !== null && location_postal_code !== "") {
+    if (res !== "") res += " ";
+    res += location_postal_code;
+  }
+  return res;
+};
+
+const money = inputDollarAmount => {
+  return inputDollarAmount.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
 };
 
 export default {
-  format
+  address,
+  money
 };
