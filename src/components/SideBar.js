6bda6f1c7d92e1358eb44d6d83fb6c43081ae1ba
Josh <maranathaman7@gmail.com>
2019-04-15 13:20:49 -0700

    reformating

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -41,6 +41,72 @@ class Sidebar extends Component {
     } = this.props;
     const { open } = this.state;
 
+    const roofingImage = (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        x="0px"
+        y="0px"
+        width="26"
+        height="26"
+        viewBox="0 0 192 192"
+      >
+        <g
+          fill="none"
+          fillRule="nonzero"
+          stroke="none"
+          strokeWidth="1"
+          strokeLinecap="butt"
+          strokeLinejoin="miter"
+          strokeMiterlimit="10"
+          strokeDasharray=""
+          strokeDashoffset="0"
+          fontFamily="none"
+          fontWeight="none"
+          fontSize="none"
+          textAnchor="none"
+        >
+          <path d="M0,192v-192h192v192z" fill="none" />
+          <g fill="#ffffff">
+            <g id="surface1">
+              <path d="M96,51.69231l-96,63.46154v25.84615l96,-63.46154l96,63.46154v-25.84615zM155.07692,51.69231v27.23077l22.15385,14.76923v-42z" />
+            </g>
+          </g>
+        </g>
+      </svg>
+    );
+
+    const windowsImage = (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        x="0px"
+        y="0px"
+        width="26"
+        height="26"
+        viewBox="0 0 192 192"
+      >
+        <g
+          fill="none"
+          fillRule="nonzero"
+          stroke="none"
+          strokeWidth="1"
+          strokeLinecap="butt"
+          strokeLinejoin="miter"
+          strokeMiterlimit="10"
+          strokeDasharray=""
+          strokeDashoffset="0"
+          fontFamily="none"
+          fontWeight="none"
+          fontSize="none"
+          textAnchor="none"
+        >
+          <path d="M0,192v-192h192v192z" fill="none" />
+          <g fill="#ffffff">
+            <path d="M19.2,15.36c-2.112,0 -3.84,1.728 -3.84,3.84v72.96h76.8v-76.8zM99.84,15.36v76.8h76.8v-72.96c0,-2.112 -1.728,-3.84 -3.84,-3.84zM15.36,99.84v72.96c0,2.112 1.728,3.84 3.84,3.84h72.96v-76.8zM99.84,99.84v76.8h72.96c2.112,0 3.84,-1.728 3.84,-3.84v-72.96z" />
+          </g>
+        </g>
+      </svg>
+    );
+
     return (
       <div className="root">
         <ClickAwayListener onClickAway={this.handleSidebarClose}>
@@ -84,39 +150,7 @@ class Sidebar extends Component {
                   className="listItem"
                   button
                 >
-                  <ListItemIcon>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      x="0px"
-                      y="0px"
-                      width="26"
-                      height="26"
-                      viewBox="0 0 192 192"
-                    >
-                      <g
-                        fill="none"
-                        fillRule="nonzero"
-                        stroke="none"
-                        strokeWidth="1"
-                        strokeLinecap="butt"
-                        strokeLinejoin="miter"
-                        strokeMiterlimit="10"
-                        strokeDasharray=""
-                        strokeDashoffset="0"
-                        fontFamily="none"
-                        fontWeight="none"
-                        fontSize="none"
-                        textAnchor="none"
-                      >
-                        <path d="M0,192v-192h192v192z" fill="none" />
-                        <g fill="#ffffff">
-                          <g id="surface1">
-                            <path d="M96,51.69231l-96,63.46154v25.84615l96,-63.46154l96,63.46154v-25.84615zM155.07692,51.69231v27.23077l22.15385,14.76923v-42z" />
-                          </g>
-                        </g>
-                      </g>
-                    </svg>
-                  </ListItemIcon>
+                  <ListItemIcon>{roofingImage}</ListItemIcon>
                   <ListItemText
                     primary="Roofing"
                     primaryTypographyProps={{ color: "inherit" }}
@@ -148,37 +182,7 @@ class Sidebar extends Component {
                   className="listItem"
                   button
                 >
-                  <ListItemIcon>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      x="0px"
-                      y="0px"
-                      width="26"
-                      height="26"
-                      viewBox="0 0 192 192"
-                    >
-                      <g
-                        fill="none"
-                        fillRule="nonzero"
-                        stroke="none"
-                        strokeWidth="1"
-                        strokeLinecap="butt"
-                        strokeLinejoin="miter"
-                        strokeMiterlimit="10"
-                        strokeDasharray=""
-                        strokeDashoffset="0"
-                        fontFamily="none"
-                        fontWeight="none"
-                        fontSize="none"
-                        textAnchor="none"
-                      >
-                        <path d="M0,192v-192h192v192z" fill="none" />
-                        <g fill="#ffffff">
-                          <path d="M19.2,15.36c-2.112,0 -3.84,1.728 -3.84,3.84v72.96h76.8v-76.8zM99.84,15.36v76.8h76.8v-72.96c0,-2.112 -1.728,-3.84 -3.84,-3.84zM15.36,99.84v72.96c0,2.112 1.728,3.84 3.84,3.84h72.96v-76.8zM99.84,99.84v76.8h72.96c2.112,0 3.84,-1.728 3.84,-3.84v-72.96z" />
-                        </g>
-                      </g>
-                    </svg>
-                  </ListItemIcon>
+                  <ListItemIcon>{windowsImage}</ListItemIcon>
                   <ListItemText
                     primary="Windows"
                     primaryTypographyProps={{ color: "inherit" }}
